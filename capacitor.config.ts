45f7099cff4717c6cb5af879fa51fbb47d08287d
JanineSuello <janinegrasyanasuello@gmail.com>
2025-01-30 08:53:59 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IT35-Lab',
+  webDir: 'dist'
+};
+
+export default config;
