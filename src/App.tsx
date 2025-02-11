fe90fd330c73cbedd994ad867b999432449f2956
LOVELAINEJOYTASIC <tasiclj07@gmail.com>
2025-02-11 23:30:04 +0800

    edited lines in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import Login from './pages/Login';
+import Menu from './pages/Menu';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -31,8 +34,6 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Login from './pages/Login';
-import Menu from './pages/Menu';
 
 setupIonicReact();
 
