3149c6021a750bc4f34e92d8d2635d0c4d26787a
JanineSuello <janinegrasyanasuello@gmail.com>
2025-02-13 16:50:49 +0800

    Updates

--- assets/keyboard2-legacy-CYdLtE5k.js
+++ assets/keyboard2-legacy-CYdLtE5k.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CBBbJ-nl.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DtVCucoB.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
