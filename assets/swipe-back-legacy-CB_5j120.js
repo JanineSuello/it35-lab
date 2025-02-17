21bc84650ec2e70cf8572aa5dedb5a30acc7dd20
JanineSuello <janinegarsyanasuello@gmail.com>
2025-02-17 08:35:11 +0800

    Updates

--- assets/swipe-back-legacy-CB_5j120.js
+++ assets/swipe-back-legacy-CB_5j120.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DtVCucoB.js","./index3-legacy-m_iGgtUU.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy--02mNXM_.js","./index3-legacy-CejXWLxd.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
