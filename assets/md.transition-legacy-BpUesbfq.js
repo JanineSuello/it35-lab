bb1b7526284bd2d64fde27fe0161371a42d7b05b
JanineSuello <janinegrasyanasuello@gmail.com>
2025-02-24 08:43:39 +0800

    Updates

--- assets/md.transition-legacy-BpUesbfq.js
+++ assets/md.transition-legacy-BpUesbfq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--02mNXM_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-Jv5qYTHf.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
